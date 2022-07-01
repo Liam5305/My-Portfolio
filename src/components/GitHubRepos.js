73ff9d3d2b5c7d36cc3c57c92383e9101ebbb2b5
Liam5305 <liamrutherford10@gmail.com>
2022-07-01 15:42:16 +0100

    Images, CSS, New Js File
    Added more images of experience, updated CSS for various pages, added new Js file 'GitHubRepos' to display certain repositories and my main repo.

---
+++ src/components/GitHubRepos.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+
+
+export default function GitHubRepos() {
+    return (
+        <>
+                <div className='githubRepo' >
+                    <a href="https://github.com/Liam5305?tab=repositories">My GitHub Repositories</a>
+                </div>
+                <div className='githubRepoRest' >
+                    <a href="https://github.com/Liam5305/NC500Hostels-SPA">NC500 Hostels SPA | React.Js </a>
+                </div>
+                <div className='githubRepoPlanner' >
+                    <a href="https://github.com/Liam5305/WebActivityPlanner">Web Activity Planner | Node.Js/Mustache </a>
+                </div>
+                <div className='githubRepoHonours' >
+                    <a href="https://github.com/Liam5305/AuctionIT_HonoursProject">Web Application Auction | React.Js </a>
+                </div>
+                <div className='githubRepoMPD' >
+                    <a href="https://github.com/Liam5305/MPD-COURSEWORK">Traffic Scotland Mobile App | Java </a>
+                </div>
+        </>
+    )
+}
